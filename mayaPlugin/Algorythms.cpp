245f759d675bc6f847285b105274a7b44b1a28a7
zielinskis <szielin1@mion.elka.pw.edu.pl>
2019-09-01 21:33:56 +0200

    dodanie randomizacji wysokosci budynkow

--- mayaPlugin/Algorythms.cpp
+++ mayaPlugin/Algorythms.cpp
@@ -22,6 +22,7 @@ std::vector<Building*> generateParisStyle(int circles) {
 		for (int i = 0; i < n; ++i) {
 			Building* b = new Building();
 			b->setAsCuboid(0,0);
+			setRandomHeight(b);
 			b->move(r, 0, 0);
 			b->rotateY(i * (360 / n));
 			buildings.push_back(b);
@@ -29,4 +30,13 @@ std::vector<Building*> generateParisStyle(int circles) {
 		r += 3;
 	}
 	return buildings;
+}
+
+Building* setRandomHeight(Building* buil) {
+	std::random_device r;
+	std::default_random_engine e1(r());
+	std::normal_distribution<> normal_dist(1,0.5);
+	double height = normal_dist(e1);
+	buil->scale(1, height, 1);
+	return buil;
 }
--- mayaPlugin/Algorythms.h
+++ mayaPlugin/Algorythms.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <vector>
+#include <random>
 
 #include "Building.h"
 
 std::vector<Building*> generateManhatanStyle(int n, int m);
 std::vector<Building*> generateParisStyle(int circles);
+Building* setRandomHeight(Building* buil);
